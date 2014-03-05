237b1322ccd5f5d60a40f7ffd4ef008c34000030
Ryan Clark <ryan.clark.j@gmail.com>
2014-03-05 13:02:08 -0700

    skeleton of in-depth profiling ideas. refs https://github.com/mapbox/tm2/issues/146

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ var request = require('request');
 var exists = fs.exists || require('path').exists;
 var numeral = require('numeral');
 var sm = new (require('sphericalmercator'))();
+var TileProfiler = require('./tile-profiler');
 var Backend = require('./backend');
 
 module.exports = Vector;
@@ -107,6 +108,7 @@ Vector.prototype.getTile = function(z, x, y, callback) {
     // as attributes of the callback function.
     var format = callback.format || this._format;
     var scale = callback.scale || this._scale;
+    var profile = callback.profile || false;
 
     var source = this;
     var drawtime;
@@ -174,9 +176,13 @@ Vector.prototype.getTile = function(z, x, y, callback) {
             } else {
                 image.encode(format, {}, function(err, buffer) {
                     if (err) return callback(err);
+                    var profiler = new TileProfiler(vtile);
+                    
                     buffer._loadtime = loadtime;
                     buffer._drawtime = (+new Date) - drawtime;
                     buffer._srcbytes = vtile._srcbytes || 0;
+                    if (profile) buffer._geometryStats = profiler.geometryStatistics();
+                    
                     return callback(null, buffer, headers);
                 });
             }
